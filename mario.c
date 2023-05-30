8f212593db80f6f6082d324087ed7e8074c1eab6
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 09:00:49 +0000

    Tue, May 30, 2023, 9:00 AM +00:00

--- mario.c
+++ mario.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    for (int i = 0; i < 3; i++)
+    int n = get_int("Size: ");
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("#");
         }

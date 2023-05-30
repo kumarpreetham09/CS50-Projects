c4b6a8f37912c96f5e08d78b4f9e718795b7a273
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 07:26:51 +0000

    Tue, May 30, 2023, 7:26 AM +00:00

--- hello.c
+++ hello.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    string first = get_string("What's your first name? ")
-    string second = get_string("What's your second name?")
+    string first = get_string("What's your first name? ");
+    string second = get_string("What's your second name?");
     printf("hello, %s %s\n", first, second);
 
 }

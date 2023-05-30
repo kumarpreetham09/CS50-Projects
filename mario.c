24305d0813de01abb0e6b5ce738e33893b48b28a
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 08:54:24 +0000

    Tue, May 30, 2023, 8:54 AM +00:00

--- mario.c
+++ mario.c
@@ -4,6 +4,10 @@ int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        
+        for (int j = 0; j < 3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }

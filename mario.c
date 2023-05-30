f296cac4c773d8f8fe2747dde2a4156558ea0621
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 09:15:29 +0000

    Tue, May 30, 2023, 9:15 AM +00:00

--- mario.c
+++ mario.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_size(void);
+void print_grid();
+
 int main(void)
 {
     // Get size of grid
     int n = get_size();
 
     //Prints grid of bricks
-    printgrid(n);
+    print_grid(n);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int get_size(void)
 {
-    int n
+    int n;
     do
     {
         n = get_int("Size: ");

179f71dc0656726957404401d8761fa1186a320f
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 07:19:56 +0000

    Tue, May 30, 2023, 7:19 AM +00:00

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello, world");
+    string answer = get_string("What's your name? ")
+    printf("hello, %s\n", answer);
+
 }

93125798418e4d801e372b1626061e91c0301505
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-05-30 09:25:05 +0000

    Tue, May 30, 2023, 9:25 AM +00:00

--- calculator.c
+++ calculator.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    long x = get_long("x: ");
-    long y = get_long("y: ");
+    float x = get_float("x: ");
+    float y = get_float("y: ");
 
-    printf("%li\n", x + y);
+    printf("%f\n", x + y);
 }

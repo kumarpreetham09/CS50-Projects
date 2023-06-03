c608f611764c5697d4a6cf828c6d85f4c08c5a0a
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-03 08:47:49 +0000

    Sat, Jun 3, 2023, 4:47 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool checksum();
-void print_brand();
+
 bool validity();
 int find_length();
 
@@ -16,7 +15,7 @@ int main(void)
 
     if (validity(n))
     {
-        print_brand(n);
+        printf("Brand");
     }
     else
     (
@@ -28,66 +27,14 @@ bool validity(long long number)
 {
     int len = find_length(number);
 
-    return (len == 13 || len == 15 || len == 16) && checksum(number);
+    return (len == 13 || len == 15 || len == 16);
 }
+
+
 
 int find_length(long long n)
 {
     int len;
     for (len = 0; n!=0; n /= 10 && len++);
     return len;
-}
-
-
-bool checksum(long long number)
-{
-    long long numb = number;
-    long long sum_even = 0;
-    long long sum_odd = 0;
-    int i;
-    long long digit;
-    int num1;
-    int num2=0;
-    long long total;
-
-    for (i = 0; numb != 0; i++ && (digit = numb % 10) &&(numb /= 10))
-    {
-        if ( i % 2 == 0)
-        {
-            long long digit_2 = digit * 2;
-            if (digit_2 > 9){
-
-                num1 = digit_2 % 10;
-                num2 /= digit_2;
-            }
-            sum_even += num1 + num2;
-
-        }
-
-        else
-        {
-            sum_odd += digit;
-        }
-
-        total = sum_even + sum_odd;
-    }
-
-    if (total % 10 == 0){
-        return true;
-    }
-
-    else {
-        return false;
-    }
-
-}
-
-void print_brand(n)
-{
-    if (n > 0){
-        printf("Hello");
-    }
-    else {
-        printf("Bye");
-    }
 }

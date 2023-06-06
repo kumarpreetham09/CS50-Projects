16f6613b1a84bfb7d1cfdd6e172e38e947f2756d
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-06 09:38:45 +0000

    Tue, Jun 6, 2023, 5:38 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    string key;
+    do
+    {
+        key = argv[1];
+    }
+    while (key>0);
 
 }

1007c5c37e1e7083c1a16bdb03e6efa5c0482645
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-06 09:47:32 +0000

    Tue, Jun 6, 2023, 5:47 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,13 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool validity();
+
 int main(int argc, string argv[])
 {
-    string key;
-    do
+    string key = argv[1];
+    if validity(key);
+
+
+}
+
+
+bool validity(string key)
+{
+    int length = strlen(key)
+    for (i = 0, i < length, i++)
     {
-        key = argv[1];
+        if (key[i] >= 65 && key[i] <= 90) || (key[i] >= 97 && key[i] <= 122)
     }
-    while (key>0);
+    if (length == 26)
 
 }

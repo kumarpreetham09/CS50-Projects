88d8869214e82d0234c667ba05da951cbcc9f6a9
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-06 10:26:48 +0000

    Tue, Jun 6, 2023, 6:26 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,12 +8,12 @@ void encrypt();
 int main(int argc, string argv[])
 {
     string key = argv[1];
-    int length = strlen(key)
+    int length = strlen(key);
 
     if (validity(key, length) == 0)
     {
-        string plain_text = get_string("plaintext: ")
-        encrypt(plain_text, key, length)
+        string plain_text = get_string("plaintext: ");
+        encrypt(plain_text, key, length);
     }
 
 
@@ -25,9 +25,22 @@ int main(int argc, string argv[])
 void encrypt(string text, string key, int length)
 {
     int i;
+    char k;
     for (i = 0; i < length; i++)
     {
+        int j = text[i];
+        if (j >= 65 && j <= 90)
+        {
+            j -= 65;
+            k = key[j];
+        }
 
+        else if (j >= 97 && j <= 122)
+        {
+            j -= 97;
+            k = key[j];
+        }
+        printf("%c", k);
     }
 }
 

ecc3392bd5c787feebdebff6c23055551338a665
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-11 09:18:18 +0000

    Sun, Jun 11, 2023, 5:18 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -134,7 +134,7 @@ bool vote(int voter, int rank, string name)
 
     if (!strcmp(name, candidates[i].name))
     {
-        preferences[voter][rank]++;
+        preferences[voter][rank] = i;
         return true;
     }
 

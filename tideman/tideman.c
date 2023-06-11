7ccc27c451e069af1327977119c7c95574c97093
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-11 06:23:45 +0000

    Sun, Jun 11, 2023, 2:23 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,6 +100,14 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            
+        }
+    }
+
     return false;
 }
 

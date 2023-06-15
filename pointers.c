90679c4c2cecebe3ec68702bcabd638a9fefe4f3
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-15 11:50:04 +0000

    Thu, Jun 15, 2023, 7:50 PM +08:00

--- pointers.c
+++ pointers.c
@@ -6,7 +6,11 @@ int main(void)
     int b = 50;
     int *c = &a;
 
-    *c = 14
+    *c = 14;
     c = &b;
     *c = 25;
+
+    printf("%p\n",&a);
+    printf("%p\n",&b);
+    printf("%p\n",&c);
 }

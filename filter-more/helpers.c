c9416bab07f7e8507fed3dd29bf764aa333deabd
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-15 15:22:22 +0000

    Thu, Jun 15, 2023, 11:22 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -25,13 +25,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = image[i][j].rgbtRed;
-            image[i][j].rgbtGreen = image[i][j].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][j].rgbtBlue;
+            image[i][j].rgbtRed = image[i][width - j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
 
 
         }
-    }
     return;
 }
 

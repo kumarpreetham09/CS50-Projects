274d67bb0128454227d05d4502b2f7c01a363ba5
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-16 11:56:25 +0000

    Fri, Jun 16, 2023, 7:56 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -47,9 +47,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
 
 
-    for (int i = 0; i < height; i++)
+    for (int i = 1; i < height-1; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 1; j < width-1; j++)
         {
             RGBTRIPLE buffer[height][width];
             int counter = 0;

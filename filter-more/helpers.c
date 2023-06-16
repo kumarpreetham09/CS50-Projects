e9f5327ab75c916576e30f815eed253e77c3d8c4
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-16 09:40:53 +0000

    Fri, Jun 16, 2023, 5:40 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -10,9 +10,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int average = ((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
-            image[i][j].rgbtRed = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
         }
     }
 

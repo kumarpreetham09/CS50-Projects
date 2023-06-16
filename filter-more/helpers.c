9a46c1224d606816482f99b832d0a02a896fd6d4
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-16 10:02:15 +0000

    Fri, Jun 16, 2023, 6:02 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include <stdio.h>
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -9,12 +10,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float red = image[i][j].rgbtRed;
-            float green = image[i][j].rgbtGreen;
-            float blue = image[i][j].rgbtBlue;
-
-            int average = (red + green + blue)/3.0;
-
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = average;
         }
     }

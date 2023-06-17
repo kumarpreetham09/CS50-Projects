d495c1bcb9df34a9b8ba1b180ce1a2158127afeb
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-17 15:34:42 +0000

    Sat, Jun 17, 2023, 11:34 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,10 +22,11 @@ int main(int argc, char *argv[])
     {
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0) )
         {
-            counter++;
+
             sprintf(file, "%03i.jpg", counter);
             FILE *img = fopen(filename. "w")
             fwrite(buffer, BYTE, BLOCK_SIZE, file);
+            counter++;
         }
 
     }

374391c4b5b33ea9cb2fab716aba25c4ba674894
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-17 15:00:07 +0000

    Sat, Jun 17, 2023, 11:00 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,9 +5,10 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover.c name\n");
+        printf("Usage: ./recover.c IMAGE\n");
         return 1;
     }
 
-    
+    FILE *file = fopen(argv[1], "r");
+
 }

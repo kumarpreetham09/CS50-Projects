da90dd42589b8907efa22fc2efc5d49c3f36c3bf
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-18 06:50:39 +0000

    Sun, Jun 18, 2023, 2:50 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     char output[10];
     bool found_img = false;
 
-    while (true)
+    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0) )
         {

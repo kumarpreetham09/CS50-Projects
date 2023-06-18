b1f88a23cea156f1f3aa01c51159bd792e5ae79b
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-18 06:57:46 +0000

    Sun, Jun 18, 2023, 2:57 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,15 +33,7 @@ int main(int argc, char *argv[])
         if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0) )
         {
             found_img = true;
-            if (counter == 0)
-            {
-                sprintf(output, "%03i.jpg", counter);
-                img = fopen(output, "w");
-                fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, img);
-                counter++;
-            }
-
-            else
+            if (counter != 0)
             {
                 fclose(img);
             }

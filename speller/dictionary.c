a4b727f55158ac44eedb48d1bb02f4f15bb1caf9
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-06-24 17:04:11 +0000

    Sun, Jun 25, 2023, 1:04 AM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -37,6 +37,7 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
+    FILE *file = fopen(dictionary, "r");
     node *n = malloc(sizeof(node))
     strcpy(n -> word;"Hello");
     n -> next = NULL

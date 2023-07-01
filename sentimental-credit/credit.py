58a92b27da3de795892bc91fb6f77fc4f7bab0ee
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-01 12:41:59 +0000

    Sat, Jul 1, 2023, 8:41 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,1 +1,15 @@
 # TODO
+from cs50 import get_string, get_int
+
+
+is_valid = True
+
+while not is_valid:
+    input = get_int("Number: ")
+    if len(input) == 13 or len(input) == 16:
+        is_valid = True
+
+
+
+def algo():
+    for

1b496729e347c6cf1e1c02b605abfb2b0c37c744
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-01 12:45:57 +0000

    Sat, Jul 1, 2023, 8:45 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,10 +6,13 @@ is_valid = True
 
 while not is_valid:
     input = get_int("Number: ")
-    if len(input) == 13 or len(input) == 16:
+    if len(input) == 13 or len(input) == 16 and algo(input):
         is_valid = True
+    else:
+        print("INVALID")
 
 
 
-def algo():
-    for
+def algo(input):
+    for i in input:
+        i

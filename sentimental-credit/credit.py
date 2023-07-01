00b2012ff59467e146fb45fad8260ff8e6e6ec3e
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-01 15:52:18 +0000

    Sat, Jul 1, 2023, 11:52 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,51 +2,55 @@
 from cs50 import get_string, get_int
 
 
-
 def main():
-    is_valid = True
-    while is_valid:
-        input = get_string("Number: ")
-        if len(str(input)) == 13 or len(str(input)) == 15 or len(str(input)) == 16:
-            if algo(input) == True:
-                is_valid = False
-                print("AMEX")
-            else:
-                print("INVALID")
+    num = input("Type:")
+    length = len(num)
+    if length == 13 or length == 15 or length == 16:
+        if algo(num) == 0:
+            print("VALID")
+        else:
+            print("INVALID")
+        return num
+    else:
+        print("INVALID")
 
 
-    if input[0] == 3 and len(input) == 15:
-        if input[1] == 4 or input[1] == 7:
-            print("AMEX")
-    elif input[0] == 4 and len(input) == 13 or len(length) == 15:
-        print("VISA")
-
-
-def algo(i):
-    input = i.split(", ")
+def algo(num):
+    def digits_of(n):
+        return[int(d) for d in str(n)]
+    digits = digits_of(num)
     odd = 0
     even = 0
-    length = len(input)
-    for i in range(length):
-        pos = (length - (i+1))
+    pos = 1
+    for i in digits[-1::-1]:
+        pos += 1
         if pos%2 == 0:
-            input[pos] += odd
+            even += i
         else:
-            buffer = input[pos] * 2
-            print(buffer)
-            # if int(buffer) > 10:
-            #     buffer_sum = buffer[0] + buffer[1]
-
-            #     buffer_sum += even
-
-            # else:
-            #     input[pos] += even
+            buffer = i * 2
+            digits2 = digits_of(buffer)
+            for j in digits2:
+                odd += j
     sum = odd + even
-    sum_length = len(sum)
-    if sum[sum_length - 1] == 0:
-        return True
+    if sum%10 == 0:
+        return 0
     else:
-        return False
+        return 1
 
+def print_func(num):
+    length = len(num)
+    print(num[0])
+    print(length)
+    if num[0] == 3 and length == 15:
+        print("hello")
+        if num[1] == 4 or num[1] == 7:
+            print("AMEX")
+        else:
+            print("INVALID")
 
-main()
+    elif num[0] == 4 and length == 13 or length == 16:
+        print("VISA")
+    else:
+        print("MASTERCARD")
+
+print_func(main())

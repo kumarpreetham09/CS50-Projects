f9a55501d616c68eb1d848e62672f66d69a0f759
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-09 11:30:12 +0000

    Sun, Jul 9, 2023, 7:30 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,6 @@
 7. SELECT passport_number, seat FROM passengers WHERE flight_id = 6;
 8. SELECT name, phone_number, license_plate FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 6);
 9. SELECT name, license_plate FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 AND duration < 60) AND license_plate in (SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 15 AND minute < 25);
+10. SELECT id, name, license_plate, passport_number FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 AND duration < 60) AND license_plate in (SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 15 AND min
+ute < 25) AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE day = 28 AND month = 7 AND y
+ear = 2021 AND atm_location ="Leggett Street" AND transaction_type = "withdraw"));

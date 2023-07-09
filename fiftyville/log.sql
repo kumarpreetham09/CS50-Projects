b26341931565fdf56d4bbcf1244b53f77650f6a0
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-09 10:13:01 +0000

    Sun, Jul 9, 2023, 6:13 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,3 +2,4 @@
 
 1. SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 AND street = "Humphrey Street";
 2. SELECT name, transcript FROM interviews WHERE day = 28 AND month = 7 AND year = 2021;
+3. 

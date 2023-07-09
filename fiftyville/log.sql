311e339c412b285763be7fab7195622629d0f8ab
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-09 10:25:05 +0000

    Sun, Jul 9, 2023, 6:25 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,5 @@
 1. SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 AND street = "Humphrey Street";
 2. SELECT name, transcript FROM interviews WHERE day = 28 AND month = 7 AND year = 2021;
 3. SELECT activity, license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 15 AND minute < 25;
-4. 
+4. SELECT account_number, person_id, creation_year FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE day = 28 AND month = 7 AND year = 2021 AND atm_location ="Leggett Street" AND transaction_type = "withdraw");
+5. 

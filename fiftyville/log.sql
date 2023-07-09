ae58e3d10b373f013c195d15781ad50e882cf442
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-09 11:16:49 +0000

    Sun, Jul 9, 2023, 7:16 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,3 +8,4 @@
 6. SELECT flight_id, destination_airport_id, hour, minute FROM flights WHERE origin_airport_id = 8 AND day = 28;
 7. SELECT passport_number, seat FROM passengers WHERE flight_id = 6;
 8. SELECT name, phone_number, license_plate FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 6);
+9. SELECT name, license_plate FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE day = 28 AND month = 7 AND year = 2021 AND duration < 60) AND license_plate in (SELECT license_plate FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND hour = 10 AND minute > 15 AND minute < 25);

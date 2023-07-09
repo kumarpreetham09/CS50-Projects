a6e50ed7df44d734f506040ca410d25bb40f66b4
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-09 10:00:19 +0000

    Sun, Jul 9, 2023, 6:00 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 crime_scene_reports
-    id :
-    day/month/year :
-    street :
+    id : 295
+    day/month/year : 28/07/2021
+    street : Humphrey Street
     description :

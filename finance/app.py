20e92b70e1a4ce9c5d5bd9d19cf910f96ca26986
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-18 13:45:05 +0000

    Tue, Jul 18, 2023, 9:45 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -80,7 +80,9 @@ def buy():
                     if cash < total_price:
                         return apology("not enough balance",400)
                     else:
+                        print(cash)
                         cash = cash - total_price
+                        print(cash)
                         db.execute("INSERT INTO history (user_id, symbol, price, shares, time) VALUES(?, ?, ?, ?, ?)", user_id, symbol, price, shares, time)
                         db.execute("UPDATE users SET cash = ? WHERE id = ?", cash, user_id)
                         flash(f"Bought {shares} shares of {symbol} at {usd(total_price)}")

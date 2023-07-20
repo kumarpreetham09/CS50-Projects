57c3754901005e36624376ba838447f42ee9d225
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-20 16:12:02 +0000

    Fri, Jul 21, 2023, 12:12 AM +08:00

--- project/app.py
+++ project/app.py
@@ -25,16 +25,9 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
         # TODO: Add the user's entry into the database
-        name = request.form.get("name")
-        month = request.form.get("month")
-        day = request.form.get("day")
-        if not name or not month or not day:
-            return redirect("/")
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
-        return redirect("/")
+            return redirect("/")
 
     else:
 

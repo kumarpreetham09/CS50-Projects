e5f7701985745b3f2d53ee358e17b2ec3069c7c0
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-20 16:38:05 +0000

    Fri, Jul 21, 2023, 12:38 AM +08:00

--- project/app.py
+++ project/app.py
@@ -6,6 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+from functools import wraps
 
 app = Flask(__name__)
 

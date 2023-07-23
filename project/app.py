f3d831ced068f46d0897e9863af68ca72c922534
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-23 10:20:52 +0000

    Sun, Jul 23, 2023, 6:20 PM +08:00

--- project/app.py
+++ project/app.py
@@ -49,24 +49,7 @@ def buy():
 def upload():
     """Show history of transactions"""
     if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        
 
         # Redirect user to home page
         return redirect("/")

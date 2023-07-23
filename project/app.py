e1ec55b2e57e49f40db92095732aef721ea9a923
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-23 11:47:07 +0000

    Sun, Jul 23, 2023, 7:47 PM +08:00

--- project/app.py
+++ project/app.py
@@ -50,7 +50,7 @@ def upload():
     """Show history of transactions"""
     if request.method == "POST":
         file1 = request.form.get("fileList")
-        
+        print(file1)
 
         # Redirect user to home page
         return redirect("/")

2d7424eb4a34fd05a6941422306a619e3075e7e4
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-25 08:50:47 +0000

    Tue, Jul 25, 2023, 4:50 PM +08:00

--- project/app.py
+++ project/app.py
@@ -9,16 +9,14 @@ from datetime import datetime
 
 from helpers import apology, login_required
 
-# Configure application
 app = Flask(__name__)
 
 
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+
 db = SQL("sqlite:///project.db")
 
 
@@ -34,71 +32,61 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-
     return render_template("index.html")
 
 
 @app.route("/search", methods=["GET", "POST"])
 @login_required
 def search():
-    """Buy shares of stock"""
-    return render_template("search.html")
+    if request.method == "POST":
+        user_id = session["user_id"]
+        url = request.form.get("url")
+        price = float(price_checker(url))
+        db.execute("INSERT INTO history (user_id, url, price) VALUES(?,?,?)",user_id, url, price,)
+        return render_template("searched.html")
+
+    else:
+        return render_template("search.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-    # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
         rows = db.execute(
             "SELECT * FROM users WHERE username = ?", request.form.get("username")
         )
 
-        # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    """Log user out"""
-
-    # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
     return redirect("/")
 
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
-    """Get stock quote."""
     if request.method == "POST":
         symbol = request.form.get("symbol")
         symbol_dict = lookup(symbol)
@@ -112,7 +100,6 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -143,3 +130,6 @@ def register():
     else:
         return render_template("register.html")
 
+
+def price_checker(url):
+    return 20.50

450f0566a5c2d03439dd217cc189bf3d2767dd99
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-26 16:05:09 +0000

    Thu, Jul 27, 2023, 12:05 AM +08:00

--- project/app.py
+++ project/app.py
@@ -3,9 +3,8 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
+from datetime import
 import requests
 from bs4 import BeautifulSoup
 from helpers import apology, login_required

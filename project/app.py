77810d602ac4c5633cdbfa46d6f07de0a9b9f559
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-07-29 05:30:19 +0000

    Sat, Jul 29, 2023, 1:30 PM +08:00

--- project/app.py
+++ project/app.py
@@ -62,7 +62,7 @@ def searched():
     data = session["data"]
     if request.method == "POST":
         user_id = session["user_id"]
-        price = float(data["price"]) * 0.895
+        price = round(float(data["price"]) * 0.897,2)
         url = data['url']
         name = data['name']
         check = db.execute("SELECT COUNT(*) AS n FROM history WHERE url = ?",url)[0]["n"]

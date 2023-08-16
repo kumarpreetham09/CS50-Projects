71ca1038e2653daaddab8a28a66ea413883707ff
apatheticaxolotl09 <123561944+apatheticaxolotl09@users.noreply.github.com>
2023-08-16 15:16:21 +0000

    Wed, Aug 16, 2023, 11:16 PM +08:00

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -58,13 +58,12 @@ def transition_model(corpus, page, damping_factor):
     a link at random chosen from all pages in the corpus.
     """
 
-    indi_remaining_probability = (1 - damping_factor) / (len(corpus))
-    indi_probability = damping_factor / (len(corpus) - 1)
-    print(indi_remaining_probability)
-    print(indi_probability)
-
     dictionary = {}
     for element in corpus:
+
+        indi_remaining_probability = (1 - damping_factor) / (len(corpus))
+        indi_probability = damping_factor / len(corpus[element])
+
         if element == page:
             dictionary.update({f"{element}": indi_remaining_probability})
         else:
@@ -88,6 +87,7 @@ def sample_pagerank(corpus, damping_factor, n):
     for i in range(n):
         dictionary = transition_model(corpus, previous_sample, damping_factor)
         previous_sample = random.choices(list(dictionary.keys()), weights=list(dictionary.values()), k=1)
+        print(dictionary)
         if i == (n-1):
             return dictionary
 
